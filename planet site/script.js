fedd27761b257ba75811c72a44931aa780e2fc8b
Henry Currie <22henrytc@spa.edu>
2020-11-09 12:41:45 -0600

    asdf

--- planet site/script.js
+++ planet site/script.js
@@ -30,7 +30,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName("container2");
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
