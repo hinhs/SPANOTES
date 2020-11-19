71ff8c5866816505a630e9e047322a63771059ad
Declan Monahan <24declanfm@spa.edu>
2020-11-19 13:42:15 -0600

    all the JS

--- Gamer_Site/scipt.js
+++ Gamer_Site/scipt.js
@@ -1,120 +1,122 @@
 var myGamePiece;
-var newGamePiece;
+var myObstacles = [];
+var myScore;
 
 function startGame() {
-    myGamePiece = new component(30, 30, "blue", 10, 180);
-    newGamePiece=new component (30, 30, "green", 10,100);
-    //Create a new component here, Let's say gren
-    
-
+    myGamePiece = new component(30, 30, "yellow", 220, 120);
+    myScore = new component("30px", "Consolas", "black", 280, 40, "text");
     myGameArea.start();
 }
 
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 460;
+        this.canvas.width = 470;
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.frameNo = 0;
         this.interval = setInterval(updateGameArea, 20);
-    },
+        },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop : function() {
+        clearInterval(this.interval);
     }
 }
 
-function component(width, height, color, x, y) {
+function component(width, height, color, x, y, type) {
+    this.type = type;
     this.width = width;
     this.height = height;
     this.speedX = 0;
-    this.speedY = 0;
+    this.speedY = 0;    
     this.x = x;
     this.y = y;    
     this.update = function() {
         ctx = myGameArea.context;
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        if (this.type == "text") {
+            ctx.font = this.width + " " + this.height;
+            ctx.fillStyle = color;
+            ctx.fillText(this.text, this.x, this.y);
+        } else {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
     }
     this.newPos = function() {
         this.x += this.speedX;
         this.y += this.speedY;        
     }
-
     this.crashWith = function(otherobj) {
-      
-      
         var myleft = this.x;
         var myright = this.x + (this.width);
         var mytop = this.y;
         var mybottom = this.y + (this.height);
-
-
         var otherleft = otherobj.x;
         var otherright = otherobj.x + (otherobj.width);
         var othertop = otherobj.y;
         var otherbottom = otherobj.y + (otherobj.height);
-        var crash = false;
-       if (myright == otherleft && mybottom >= othertop && mytop <= otherbottom) {
-          crash = true;
+        var crash = true;
+        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
+            crash = false;
         }
-        if (myleft == otherright && mybottom >= othertop && mytop <= otherbottom) {
-          crash = true;
-        }
-         if (mytop == otherbottom && myright >= otherleft && myleft <= otherright) {
-          crash = true;
-        }
-         if (mybottom == othertop && myright >= otherleft && myleft <= otherright) {
-          crash = true;
-        }
-      
         return crash;
     }
-
-    this.randomPos = function() {
-      this.x = Math.floor(Math.random() * 200) + 1;;
-      this.y = Math.floor(Math.random() * 200) + 1;;
-      
-      return ;
-    }
-
-      
 }
 
 function updateGameArea() {
+    var x, height, gap, minHeight, maxHeight, minGap, maxGap;
+    for (i = 0; i < myObstacles.length; i += 1) {
+        if (myGamePiece.crashWith(myObstacles[i])) {
+            myGameArea.stop();
+            return;
+        } 
+    }
     myGameArea.clear();
+    myGameArea.frameNo += 1;
+    if (myGameArea.frameNo == 1 || everyinterval(100)) {
+        x = myGameArea.canvas.width;
+        minHeight = 0;
+        maxHeight = 250;
+        height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
+        minGap = 50;
+        maxGap = 80;
+        gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
+        myObstacles.push(new component(height, 10 , "brown", 0, 0));
+        myObstacles.push(new component(x - height + gap ,10, "brown", x-height, 0));
+    }
+    for (i = 0; i < myObstacles.length; i += 1) {
+        myObstacles[i].speedY = myGameArea.frameNo/1000 + 1;
+        myObstacles[i].newPos();
+        myObstacles[i].update();
+    }
+    myScore.text="SCORE: " + myGameArea.frameNo;
+    myScore.update();
     myGamePiece.newPos();    
     myGamePiece.update();
-    newGamePiece.newPos();
-    newGamePiece.update();
-    
-    //Edit this if statement if needed
-    if (myGamePiece.crashWith(newGamePiece)) {
-      newGamePiece.randomPos();
+}
 
-      return;
-    } 
+function everyinterval(n) {
+    if ((myGameArea.frameNo / n) % 1 == 0) {return true;}
+    return false;
 }
-//the purpose of the update is to put the background over the old image of the square, so that only one square is on the page at a time, like a flip book
 
-//Hmmmm how would you mess with the speed so your thing can move up and down?
-function moveup() {
-  myGamePiece.speedY -= 1;
-  myGamePiece.speedX=0 
-}
-function movedown(){
-  myGamePiece.speedY =1;
-  myGamePiece.speedX=0
-}
-function stop(){
-  myGamePiece.speedY=0;
-  myGamePiece.speedX=0;
-}
-function moveleft(){
-  myGamePiece.speedX-=1;
-  myGamePiece.speedY = 0;
-}
-function moveright(){
-  myGamePiece.speedX=1;
-  myGamePiece.speedY=0;
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '37') {
+       myGamePiece.speedX = -1
+    }
+    else if (e.keyCode == '39') {
+      myGamePiece.speedX = 1
+    }
+    else if (e.keyCode == '40') {
+        myGamePiece.speedX = 0
+    }
+
 }
